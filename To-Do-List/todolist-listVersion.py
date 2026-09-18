14945b13d7c3c2b794d3f349683eb5f5be5cb3fa
sallmaahmedd <329993090+sallmaahmedd@users.noreply.github.com>
2026-09-18 20:02:58 +0300

    Simplify task viewing logic in todolist-listVersion.py

--- To-Do-List/todolist-listVersion.py
+++ To-Do-List/todolist-listVersion.py
@@ -23,6 +23,9 @@ if os.path.exists(filename):
     (the variables between for and in, receiving new values for each iteration, so you can use them inside the loop body)
     receiving each whole pair as a tuple, it splits each pair into two separate named variables  """
 def view():
+    """ Empty list evaluates to False in boolean and non-empty is True """
+    if not tasks:
+                print("No Tasks Yet!")
     for index,task in enumerate(tasks, start=1):
         print(f"{index}.{task}")
 
@@ -39,11 +42,7 @@ while True:
 
 
     if choice=="view" or choice=="1" or choice=="1.view":
-        """ Empty list evaluates to False in boolean and non-empty is True """
-        if not tasks:
-            print("No Tasks Yet!")
-        else:
-            view()
+        view()
 
     elif choice in ("add","2.add","2"):
         new_task=input("Enter your task: ")
