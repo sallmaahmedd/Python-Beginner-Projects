1ff0f53e6732599f6e026cdff1fbc67b396403e0
sallmaahmedd <329993090+sallmaahmedd@users.noreply.github.com>
2026-09-21 10:38:43 +0300

    Password Generator

--- Password-Generator/passwordgenerator.py
+++ Password-Generator/passwordgenerator.py
@@ -30,14 +30,17 @@ def build_pool():
         pool += string.digits
     if include_symbols:
         pool += string.punctuation
-    if pool == "":
-        print("You must select at least one character type!")
 
-
-length=int(input_length)
+    
 build_pool()
-""" random.choice(): returns a list of randomly selected elements from the pool, with k specifying the number of elements to select. 
-    "".join(...): takes the list of characters and turns them into one string, with "" (empty string) between each character, meaning no spaces between them. 
-"""
-password = "".join(random.choices(pool, k=input_length))
-print(f"Your generated password is: {password}")
+
+if pool == "":
+    print("You must select at least one character type!")
+
+else:
+    length=int(input_length)
+    """ random.choice(): returns a list of randomly selected elements from the pool, with k specifying the number of elements to select. 
+        "".join(...): takes the list of characters and turns them into one string, with "" (empty string) between each character, meaning no spaces between them. 
+    """
+    password = "".join(random.choices(pool, k=input_length))
+    print(f"Your generated password is: {password}")
